67e00255c935728dd3cbd9274540c3a316235cc1
sssadboy <Fifashf@yandex.ru>
2023-05-13 14:14:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,52 +1,61 @@
 import nextcord
-import logging
-import os
 from nextcord.ext import commands
 import redis
-
-r = redis.Redis(
-    host='redis-16728.c266.us-east-1-3.ec2.cloud.redislabs.com',
-    port=16728,
-    password='purple_love0816',
-    charset="utf-8",
-    decode_responses=True
-    )
-
-intents = nextcord.Intents.default()
-intents.message_content = True
-
-bot = commands.Bot(command_prefix="!",intents=intents)
-
-
-# r.set('balance', '0')  # Установка значения
-# user_id = r.get('index')  # Получение значения
+import json
+import os
+from dotenv import load_dotenv
 
 
 # @bot.slash_command(description="Show your profile!"
 # async def profile(interaction: nextcord.Interaction):
 #     await interaction.send(file=nextcord.File('picture.png'))
 
+intents = nextcord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="!",intents=intents)
+
+def encode_data(payload):
+    return f"{payload}".encode()
+
+
+def decode_data(payload):
+    return json.load(payload)
+
+
 @bot.event
 async def on_message(message):
     if not message.author.bot:
         user_id = message.author.id
-        r.set(user_id, bytes({'balance': 0}))
+        r.set(user_id, encode_data({'balance': 0}))
 
 
 @bot.slash_command(description="Show your balance!")
 async def my_balance(interaction: nextcord.Interaction):
     if not interaction.user.bot:
         user_id = interaction.user.id
-    await interaction.send("Ваш баланс составляет:{}".format(r.get(user_id)))
+    await interaction.send("Ваш баланс составляет:{}".format(decode_data(r.get(user_id))['balance']))
 
 
 @bot.command()
 async def bonus(interaction: nextcord.Interaction):
     if not interaction.user.bot:
         user_id = interaction.user.id
-    r.incrby(user_id, 200)
-    await interaction.send("На ваш баланс зачислено 200 коинов!")
+    user_data = r.get(user_id)
+    balance = user_data['balance']
+    balance += 600
+    user_data['balance'] = balance
+    r.set(user_id,encode_data(user_data))
+    await interaction.send("На ваш баланс зачислено 600 коинов!")
 
 
 if __name__ == '__main__':
-    bot.run('MTEwNTE5NzM1NDUwOTk0NjkxMA.G6KVPw.DUDQ1MVzIxw662YHS0wBCN8-ZPDJn7W0eNI6Is')
+    r = redis.Redis(
+    host='redis-16728.c266.us-east-1-3.ec2.cloud.redislabs.com',
+    port=16728,
+    password='purple_love0816',
+    charset="utf-8",
+    decode_responses=True
+    )
+    load_dotenv()
+    token = os.environ['DS_TOKEN']
+    bot.run(token)
